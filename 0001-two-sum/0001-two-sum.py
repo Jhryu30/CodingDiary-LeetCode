697f73318e717170c8513348af06e50d444ad6b1
Jhryu30 <100555769+Jhryu30@users.noreply.github.com>
2023-11-02 21:52:29 +0900

    Time: 58 ms (88.87%), Space: 17.5 MB (38.74%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,9 @@
+# [python with alg. 175] 3. save time using dictionary
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if nums[i]+nums[j] == target:
-                    return [i,j]
+        nums_map = {num:i for i,num in enumerate(nums)}
+
+        for i,num in enumerate(nums):
+            if target-num in nums_map and not i==nums_map[target-num]:
+                return nums.index(num), nums_map[target-num]
