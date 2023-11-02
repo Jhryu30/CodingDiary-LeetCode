a5f5cbe2b0a8ebb873f3242d075cc1a5b9049748
Jhryu30 <100555769+Jhryu30@users.noreply.github.com>
2023-11-02 19:37:41 +0900

    Time: 46 ms (35.19%), Space: 16.3 MB (34.55%) - LeetHub

--- 0837-most-common-word/0837-most-common-word.py
+++ 0837-most-common-word/0837-most-common-word.py
@@ -1,25 +1,9 @@
-from collections import defaultdict
+# 1. list comprehesion, counter
 
 class Solution:
-    def mostCommonWord(self, paragraph: str, banned: list[str]) -> str:
-        my_dict = defaultdict(int)
-        
-        paragraph_word = []
-        for para in paragraph.lower().split(' '):
-          para_temp = [p for p in para.split(',') if len(p)>0]
-          paragraph_word += para_temp
-          
-        for para in paragraph_word:
-            word_lst = [p for p in para.lower() if p.isalnum()]
-            word = ''.join(word_lst)
-            my_dict[word] += 1
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        words = [word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]
 
-        for banned_word in banned:
-            my_dict[banned_word] = 0
+        counts = collections.Counter(words)
 
-        my_word = [[w,c] for w,c in my_dict.items()]
-        my_word.sort(key=lambda x:x[1], reverse=True)
-        
-        answer = my_word[0][0]
-
-        return answer
+        return counts.most_common(1)[0][0]
